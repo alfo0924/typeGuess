7e93fc1efd49ca7444f884a8ba1d0817c183afff
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-05-02 15:59:06 +0800

    Create typeGuess.java
    added

---
+++ typeGuess.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+public class typeGuess 
+{
+  public static void main(String[] args) {
+    
+
+    Scanner scanner=new Scanner(System.in);
+    while(true)
+    {
+
+        int type=scanner.nextInt();
+        if(type==-1)
+        {
+            break;
+        }
+
+
+
+    }
+
+
+
+
+
+
+  }
+
+
+
+    
+}
